cc8e59a60f93fd4804fbb2cc5ead094baff10ea2
GrimCreeperyt <grimcreeperyt@gmail.com>
2023-11-30 21:30:11 -0500

    js works rfor some reason, so commit time ig

--- script.js
+++ script.js
@@ -1,2 +1,30 @@
-var audio = document.getElementsByTagName("audio");
-audio.play();
+/*var audio = document.getElementsByTagName("audio")[0];
+audio.play();*/
+
+var playBtn = document.getElementById('play'),
+  resetBtn = document.getElementById('reset'),
+  hearbeat = document.getElementById('heartbeat')
+	audios = document.querySelectorAll('audio');
+console.log(audios);
+
+
+playBtn.addEventListener('mouseover', function() {
+[].forEach.call(audios, function(audio) {
+  // do whatever
+  audio.play();
+});
+}, false);
+
+playBtn.addEventListener('mouseleave', function() {
+  heartbeat.pause();
+  heartbeat.currentTime = 0;
+}, false);
+
+resetBtn.addEventListener('mouseover', function() {
+    heartbeat.play();
+}, false);
+
+resetBtn.addEventListener('mouseleave', function() {
+  heartbeat.pause();
+  heartbeat.currentTime = 0;
+}, false);
