9a52ba366dcd89a1996c36530fad6d7e84bfea12
GrimCreeperyt <grimcreeperyt@gmail.com>
2023-12-03 23:44:04 -0500

    finished early careerc bd almost late ouifw

--- script.js
+++ script.js
@@ -1,18 +1,19 @@
 // Allows for audio to be played
 
+// Defines variables
 var playBtn = document.getElementById('play'),
   cluck = document.getElementById('cluck')
 	audios = document.querySelectorAll('audio');
 console.log(audios);
 
-
+// Plays audio when hovered over
 playBtn.addEventListener('mouseover', function() {
 [].forEach.call(audios, function(audio) {
-  // do whatever
   audio.play();
 });
 }, false);
 
+// Stops playing audio when no longer hovered over
 playBtn.addEventListener('mouseleave', function() {
   cluck.pause();
   cluck.currentTime = 0;
